9b1582551279b1ee0c5167d73b9b3a2745fd62a0
Lasha Dolidze <lashad@mac.com>
2026-06-24 22:53:22 +0400

    Add React Router v7 routes

---
+++ src/app/router.tsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter, Navigate, Outlet } from 'react-router';
+
+import { ItemPage } from '@/pages/item/ItemPage';
+import { ListPage } from '@/pages/list/ListPage';
+import { LoginPage } from '@/pages/login/LoginPage';
+
+function RootLayout() {
+  return (
+    <main className="min-h-screen">
+      <Outlet />
+    </main>
+  );
+}
+
+export const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <ListPage /> },
+      { path: '/item/:id', element: <ItemPage /> },
+      { path: '/item', element: <ItemPage /> },
+      { path: '/new', element: <ItemPage /> },
+      { path: '/login', element: <LoginPage /> },
+      { path: '*', element: <Navigate to="/" replace /> },
+    ],
+  },
+]);
